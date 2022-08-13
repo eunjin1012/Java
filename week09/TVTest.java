bc02a8498f472c4d998b3acd96cff5cba2751d27
eunjin1012 <72156494+eunjin1012@users.noreply.github.com>
2022-08-13 23:32:13 +0900

    TVTest

---
+++ week09/TVTest.java
@@ -0,0 +1,14 @@
+package week09;
+
+public class Test01 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		TV myTV=new TV("LG", 2019, 50);
+		myTV.show();
+		myTV=new TV("삼성", 2018, 55);
+		myTV.show();
+
+	}
+
+}
