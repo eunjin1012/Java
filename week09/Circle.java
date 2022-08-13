c0801ace56f15aa017dfdd0a312b400e4982f0e4
eunjin1012 <72156494+eunjin1012@users.noreply.github.com>
2022-08-13 23:28:01 +0900

    Circle

---
+++ week09/Circle.java
@@ -0,0 +1,19 @@
+package week09;
+
+public class Circle {
+	double radius;
+	double PI;
+	
+	public Circle(double radius) {
+		super();
+		this.radius = radius;
+	}
+
+	public Circle() {
+		super();
+		this.radius=2.8;
+		this.PI=3.141592;
+	}
+	
+
+}
