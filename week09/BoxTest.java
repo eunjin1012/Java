219fa584a1b8e05ba1c6a117e017ecd9caeeaa0f
eunjin1012 <72156494+eunjin1012@users.noreply.github.com>
2022-08-13 23:27:37 +0900

    BoxTest

---
+++ week09/BoxTest.java
@@ -0,0 +1,11 @@
+package week09;
+
+public class BoxTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Box myBox=new Box(10,3.14);
+		myBox.show();
+	}
+
+}
