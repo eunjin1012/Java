6ac27821a543bf38950a89b0688433e615f6691e
eunjin1012 <72156494+eunjin1012@users.noreply.github.com>
2022-08-13 23:30:03 +0900

    PlaneTest

---
+++ week09/PlaneTest.java
@@ -0,0 +1,21 @@
+package week09;
+
+public class PlaneTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Plane plane1=new Plane(1, "aa", 200);
+		Plane plane2=new Plane(2, "bb");
+		Plane plane3=new Plane();
+		plane1.setPlanes(0);
+		plane1.getPlanes();
+		plane3.setNum(3);
+		plane3.setModel("cc");
+		plane3.setPnum(150);
+		System.out.println(plane1);
+		System.out.println(plane2);
+		System.out.println(plane3);
+		
+	}
+
+}
