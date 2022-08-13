fc6653a8000ff577ffc60907771c2256813614c9
eunjin1012 <72156494+eunjin1012@users.noreply.github.com>
2022-08-13 23:28:42 +0900

    Cylinder

---
+++ week09/Cylinder.java
@@ -0,0 +1,15 @@
+package week09;
+
+public class Cylinder {
+	double height;
+	double radius;
+	double PI;
+	public Cylinder(double height, double radius, double pI) {
+		super();
+		this.height = height;
+		this.radius = radius;
+		PI = pI;
+	}
+	
+
+}
