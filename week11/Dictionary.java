4cb77019eb7adfe73a59903f77f210c5fd6ac672
eunjin1012 <72156494+eunjin1012@users.noreply.github.com>
2022-08-14 15:41:41 +0900

    Dictionary

---
+++ week11/Dictionary.java
@@ -0,0 +1,14 @@
+package week11;
+
+public class Dictionary {
+	private static String [] kor= {"사랑", "아기", "돈", "미래", "희망"};
+	private static String [] eng= {"love", "baby", "money", "future", "hope"};
+	public static String convert(String word) {
+		System.out.println("한영, 영한 단어 검색 프로그램(종료시 stop입력)");
+		while(true) {
+			kor=eng;
+			
+		}
+	}
+
+}
