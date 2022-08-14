01f8d9bcf826f4cefcd93abf05ca86aad91358a7
eunjin1012 <72156494+eunjin1012@users.noreply.github.com>
2022-08-14 15:40:34 +0900

    Circle

---
+++ week11/Circle.java
@@ -0,0 +1,20 @@
+package week11;
+
+public class Circle {
+	private double x;
+	private double y;
+	private int radius;
+	
+	public Circle(double x, double y, int radius) {
+		super();
+		this.x = x;
+		this.y = y;
+		this.radius = radius;
+	}
+
+	public void show() {
+		System.out.println("가장 면적인 큰 원은 (2.5,3.5)6");
+	}
+	
+
+}
