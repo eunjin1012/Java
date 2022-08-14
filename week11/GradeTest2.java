5d6069e8081ba5be9a0b720d3162e00cea1ea5de
eunjin1012 <72156494+eunjin1012@users.noreply.github.com>
2022-08-14 15:42:25 +0900

    GradeTest2

---
+++ week11/GradeTest2.java
@@ -0,0 +1,15 @@
+package week11;
+
+public class GradeTest2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a=Integer.parseInt(args[0]);
+		int b=Integer.parseInt(args[1]);
+		int c=Integer.parseInt(args[2]);
+		int average=(a+b+c)/3;
+		System.out.println("평균은"+average);
+
+	}
+
+}
