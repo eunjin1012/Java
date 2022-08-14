13c1ac8695c5409170a6c5ce9de07b3b87f87ca7
eunjin1012 <72156494+eunjin1012@users.noreply.github.com>
2022-08-14 15:42:58 +0900

    args[]

---
+++ week11/args[].java
@@ -0,0 +1,14 @@
+package week11;
+
+public class Test01 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println(args[0]+"에서 만든 "+args[1]+"년형 "+args[2]+"인치 TV");
+		String a=args[0];
+		int b=Integer.parseInt(args[1]);
+		int c=Integer.parseInt(args[2]);
+
+	}
+
+}
