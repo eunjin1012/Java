66130408a1d4fd91e20334d02e6e7ed922864bce
eunjin1012 <72156494+eunjin1012@users.noreply.github.com>
2022-08-15 21:56:29 +0900

    ColorPointEx1

---
+++ week12/ColorPointEx1.java
@@ -0,0 +1,39 @@
+package week12;
+
+class Point{
+	private int x, y;
+
+	public Point(int x, int y) {
+		super();
+		this.x = x;
+		this.y = y;
+	}
+	public void showPoint() {
+		System.out.println("("+x+","+y+")");
+	}
+}
+class ColorPoint extends Point{
+	private String color;
+
+	public ColorPoint(int x, int y, String color) {
+		super(x, y);
+		this.color = color;
+	}
+	public void showColorPoint() {
+		System.out.print(color);
+		showPoint();
+	}
+}
+public class ColorPointEx1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Point p=new Point(1,2);
+		p.showPoint();
+		
+		ColorPoint cp=new ColorPoint(3,4,"red");
+		cp.showColorPoint();
+
+	}
+
+}
