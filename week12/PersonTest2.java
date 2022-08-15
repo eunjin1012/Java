ffda9efa860a073ae8b20801fe09bbf5cc0b8b8c
eunjin1012 <72156494+eunjin1012@users.noreply.github.com>
2022-08-15 21:57:35 +0900

    PersonTest2

---
+++ week12/PersonTest2.java
@@ -0,0 +1,14 @@
+package week12;
+
+public class PersonTest2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Customer c1=new Customer("홍길동", "아산시 호서대학교", "041 540 5114");
+		Customer c2=new Customer("이순신", "천안시 호서대학교", 1234, 540);
+		System.out.println("객체 1 : \n"+c1);
+		System.out.println("객체 2 : \n"+c2);
+		Customer c3=new Customer("장영실");
+		System.out.println("객체 3 : \n"+c3.name);
+	}
+	}
