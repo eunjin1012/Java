97b6b9eb25dbc20c7a802abbd40050dc16cd1232
eunjin1012 <72156494+eunjin1012@users.noreply.github.com>
2022-08-18 17:54:54 +0900

    Shapes

---
+++ week15/Shapes.java
@@ -0,0 +1,12 @@
+package week15;
+
+public class Shapes {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Shape donut=new Circle(10);
+		donut.redraw();
+		System.out.println("면적은"+donut.getArea());
+	}
+
+}
