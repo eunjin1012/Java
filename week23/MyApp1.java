0711d390d813629a4ee6d75fb5c701f8ec3194d3
eunjin1012 <72156494+eunjin1012@users.noreply.github.com>
2022-08-18 18:22:16 +0900

    MyApp1

---
+++ week23/MyApp1.java
@@ -0,0 +1,15 @@
+package week23;
+
+import javax.swing.*;
+
+public class MyApp1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		JFrame f=new JFrame();
+		f.setTitle("첫 번째 프레임");
+		f.setSize(300, 300);
+		f.setVisible(true);
+	}
+
+}
