68cab199aaab91de7f52a52f2e606772a5cb522d
eunjin1012 <72156494+eunjin1012@users.noreply.github.com>
2022-08-18 17:56:36 +0900

    converter

---
+++ week16/converter.java
@@ -0,0 +1,12 @@
+package week16;
+
+public class Test01 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		double a=3.14;
+		
+		System.out.println(a+"를 16진수로 표현: "+Double.toHexString(a));
+	}
+
+}
