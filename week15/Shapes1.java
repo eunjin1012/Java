aaf0ff3486c1f25dc5da2f68b504ea4045175dbc
eunjin1012 <72156494+eunjin1012@users.noreply.github.com>
2022-08-18 17:55:11 +0900

    Shapes1

---
+++ week15/Shapes1.java
@@ -0,0 +1,16 @@
+package week15;
+
+public class Shapes1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Shape [] list=new Shape[3];
+		list[0]=new Circle(10);
+		list[1]=new Oval(20,30);
+		list[2]=new Rect(10,40);
+		
+		for(int i=0; i<list.length; i++)list[i].redraw();
+		for(int i=0; i<list.length; i++)System.out.println("면적은"+list[i].getArea());
+	}
+
+}
