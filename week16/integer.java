c321af9349afe82380c9357c2802aa6b54faf1a0
eunjin1012 <72156494+eunjin1012@users.noreply.github.com>
2022-08-18 17:56:13 +0900

    integer

---
+++ week16/integer.java
@@ -0,0 +1,16 @@
+package week16;
+
+public class integer {
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int i=1234;
+		
+		System.out.println(i+"을 2진수로 표현 : "+Integer.toBinaryString(i));
+		System.out.println(i+"을 8진수로 표현 : "+Integer.toOctalString(i));
+		System.out.println(i+"을 16진수로 표현 : "+Integer.toHexString(i));
+		
+
+	}
+
+}
