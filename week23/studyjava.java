a713ad9920fd10fefb4ea7818c46b3883c9bf862
eunjin1012 <72156494+eunjin1012@users.noreply.github.com>
2022-08-18 18:23:20 +0900

    studyjava

---
+++ week23/studyjava.java
@@ -0,0 +1,16 @@
+package week23;
+
+import java.awt.*;
+import javax.swing.*;
+
+public class studyjava {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		JFrame f=new JFrame();
+		f.setTitle("Let's study java");
+		f.setSize(400, 200);
+		f.setVisible(true);
+	}
+
+}
